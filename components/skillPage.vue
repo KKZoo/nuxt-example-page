25f997d9499b0a02ffbfe95c499028111ef975f7
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2020-08-28 22:11:03 +0900

    skill pageの追加

---
+++ components/skillPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <h2 class="skill-page-title">
+            Skills
+        </h2>
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="~/assets/vue_logo.png" class="vue-logo">
+                Vue.js
+            </div>
+            <div class="col-sm-4">
+                <img src="~/assets/css_logo.svg" class="css-logo">
+                CSS
+            </div>
+            <div class="col-sm-4">
+                <img src="~/assets/bootstrap_vue_logo.png" class="bootstrap-logo">
+                Bootstrap Vue
+            </div>
+        </div>
+        </div>
+    </section>
+</template>
+
+<style>
+.skill-page-title{
+    left: 0%;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    margin-left: 20px;
+}
+
+.vue-logo{
+    width: 100%;
+}
+
+.css-logo{
+    width: 100%;
+}
+
+.bootstrap-logo{
+    width: 100%;
+}
+</style>>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <section>
     <navbar />
     <topPage />
+    <skillPage />
   </section>
 </template>
 
 <script>
 import navbar from '~/components/navbar.vue'
 import topPage from '~/components/topPage.vue'
+import skillPage from '~/components/skillPage.vue'
 
 export default {
     components: {
         navbar,
-        topPage
+        topPage,
+        skillPage
     }      
 }
 </script>>
