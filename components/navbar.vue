2fc75a8cb604edd9525d2d0da4dc60af5a8c37f3
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2020-08-31 02:09:08 +0900

    navbarデザインの微調整

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar fixed="top" toggleable="lg" type="dark" variant="primary">
+    <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand href="/">
         KKZoo
       </b-navbar-brand>
