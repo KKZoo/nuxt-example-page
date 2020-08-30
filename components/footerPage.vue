b65b17e4347575aab3cdabc397e69e840ab31841
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2020-08-30 21:34:25 +0900

    footerデザインの追加

--- components/footerPage.vue
+++ components/footerPage.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="font-small pt-0">
+  <b-card-footer class="font-small pt-0">
     <div class="footer-copyright text-center py-3">
       <b-container fluid>
         &copy; 2020 KKZoo
       </b-container>
     </div>
-  </div>
+  </b-card-footer>
 </template>
